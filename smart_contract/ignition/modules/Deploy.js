1bc0296366c75531a262de0dbaf1281833d5ac73
Kingson.Arum <nwajiudo@gmail.com>
2024-11-12 11:43:23 +0100

    adding dist file

--- smart_contract/ignition/modules/Deploy.js
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-const TransactionsModule = buildModule("TransactionsModule", (m) => {
-  const token = m.contract("Transactions");
-
-  return { token };
-});
-
-module.exports = TransactionsModule;
